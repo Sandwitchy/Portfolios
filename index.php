fbb4001717f141b67f8e0cf21297ce56b6ae2c82
Sandwitchy <sandwitchy@xperidia.com>
2018-12-22 17:28:25 +0100

    :imsexy:

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>SpirIT 2.0</title>
+  <meta name="description" content="devellopeur portfolios">
+  <meta name="author" content="FRATANI Andréa">
+  <title>Andréa FRATANI</title>
   <link rel="stylesheet" href="assets/css/color.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono|Inconsolata" rel="stylesheet">
   <link href="https://cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css" rel="stylesheet">
